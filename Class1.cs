ad5601f517744dff9823bfb51ffd0ec98afc0432
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-05-16 18:31:48 -0500

    Proyecto Beta

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Class1
+    {
+        public double Suma(double num1, double num2)
+        {
+            double suma;
+            suma = num1 + num2;
+            return suma;
+        }
+        public double Resta(double num1,double num2)
+        {
+            double resta;
+            resta = num1 - num2;
+            return resta;
+        }
+        public double Multiplicacion(double num1,double num2)
+        {
+            double mult;
+            mult = num1 * num2;
+            return mult;
+        }
+        public double Division(double num1,double num2)
+        {
+            double division;
+            division = num1 / num2;
+            return division;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Calculadora;
+using System;
+using System.Net.Http.Headers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int opcion;
+        double num1, num2;
+        var op = new Class1();
+
+        do
+        {
+            Console.WriteLine("Ingrese una opción");
+            Console.WriteLine("1.Suma  2.Resta  3.Multiplicación  4.Division  0.Salir");
+            opcion = int.Parse(Console.ReadLine());
+
+            if (opcion!=0)
+            {
+                if (opcion>=1 && opcion<=4)
+                {
+                    Console.WriteLine("Ingrese el primer número: ");
+                    num1 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese el segundo número: ");
+                    num2 = double.Parse(Console.ReadLine());
+
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("La suma de " + num1 + " y " + num2 + " es: " + op.Suma(num1,num2));
+                            break;
+                        case 2:
+                            Console.WriteLine("La resta de " + num1 + " y " + num2 + " es: " + op.Resta(num1,num2));
+                            break;
+                        case 3:
+                            Console.WriteLine("La multiplicación de " + num1 + " y " + num2 + " es: " + op.Multiplicacion(num1,num2));
+                            break;
+                        case 4:
+                            Console.WriteLine("La división será: " + num1 + "/" + num2);
+                            if (num2 != 0)
+                            { 
+                                Console.WriteLine("La división de " + num1 + " y " + num2 + " es: " + op.Division(num1,num2));
+                            }
+                            else
+                            {
+                                Console.WriteLine("El denominador debe ser mayor que 0");
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("La opción debe estar entre 1 y 4 o 0 para salir");
+                }
+            }
+        } while (opcion != 0);
+    }
+}
